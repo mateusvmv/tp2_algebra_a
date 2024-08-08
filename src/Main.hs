bbbb2c62328a37d4516e570f9f9eee50ffbf00b6
mateusvmv <mateusv251@gmail.com>
2024-08-08 10:09:04 -0300

    Dynamic matrix

--- src/Main.hs
+++ src/Main.hs
@@ -9,29 +9,34 @@ import Data.Fixed
 import Data.Maybe
 import Data.Bifunctor
 
-upperEchelon :: UArray Int Int64 -> UArray Int Int64
-upperEchelon mat = listArray (0, 63) (reverse psList) where
+upperEchelon :: UArray (Int, Int) Bool -> UArray (Int, Int) Bool
+upperEchelon mat = listArray ((0, 0), (lines, cols)) (concatMap elems . reverse $ psList) where
+    (lines, cols) = snd $ bounds mat
+    lineList = [listArray (0, cols) [mat!(i,j) | j <- [0..cols]] | i <- [0..lines]]
     findPivot [] _ = (Nothing, [])
     findPivot (r:rs) k
-        | testBit r k = (Just r, rs)
+        | r!k = (Just r, rs)
         | otherwise = (r', r:mat') where (r', mat') = findPivot rs k
-    takePivot p k r = if testBit r k then xor r p else r
-    nextPivot (mat, ps) k = case findPivot mat k of
+    takePivot p k r = if r!k then listArray (0, cols) [xor (r!j) (p!j) | j <- [0..cols]] else r
+    nextPivot :: ([UArray Int Bool], [UArray Int Bool]) -> Int -> ([UArray Int Bool], [UArray Int Bool])
+    nextPivot (lines, ps) k = case findPivot lines k of
         (Just p, mat') -> (map (takePivot p k) mat', p:ps)
-        (Nothing, mat') -> (mat', 0:ps)
-    (_, psList) = foldl nextPivot (elems mat, []) [0..63]
+        (Nothing, mat') -> (mat', array (0, cols) []:ps)
+    (_, psList) = foldl nextPivot (lineList, []) [0..cols]
 
-solve :: UArray Int Int64 -> Int64
+solve :: UArray (Int, Int) Bool -> UArray Int Bool
 solve mat = s where
     ue = upperEchelon mat
-    solveBit (i, r) s
+    (lines, cols) = snd $ bounds ue
+    solveLine :: UArray Int Bool -> Int -> UArray Int Bool
+    solveLine s i
         -- Unbound pivot, set to one
-        | r == 0 = setBit s i
+        | not $ ue!(i,i) = s // [(i, True)]
         -- The pivot is set because it must complement the set variables and result in zero
-        | odd $ popCount ((r .&. s) .>>. (i+1)) = setBit s i
+        | foldl xor False [ue!(i,j) && s!j | j <- [0..i-1]] = s // [(i, True)]
         -- If there is an even amount of set variables, the pivot is unset
         | otherwise = s
-    s = foldr solveBit 0 (assocs ue)
+    s = foldl solveLine (array (0, cols) []) [0..min lines cols]
 
 ceilSqrt :: Integer -> Integer
 ceilSqrt n = heron (fromInteger n :: Deci) where
@@ -41,14 +46,28 @@ ceilSqrt n = heron (fromInteger n :: Deci) where
         | otherwise = ceiling xi
         where d = (fromInteger n / xi - xi) / 2
 
-primes = 2 : sieve [3, 5..] where
-    sieve (p:xs) = p : filter (\x -> mod x p /= 0) xs
+-- Segmento do Crivo de Eratosthenes
+-- Coprimos de primos em ps ∪ {2} entre a e b, a≅1 mod 2 e b≅1 mod 2
+-- Em O(n*log(log(n))) com n = b-a
+sieveSeg a' b' ps = [i*2+1 | i <- [a..b], coprime ! i] where
+    a = shiftR a' 1; b = shiftR b' 1
+    muls p = [l, l+p .. b] where l = shiftR (p * ((a'+p-1) `div` p .|. 1)) 1
+    coprime = accumArray (\_ b -> b) True (a, b) (map (,False) $ concatMap muls ps) :: UArray Integer Bool
 
+-- Primos
+-- Gera e armazena primos um segmento por vez
+-- Em O(n*log(log(n))) com n sendo o maior primo gerado
+primes :: [Integer] = [2,3] ++ sieve 5 where
+    sieve n = sieveSeg n top ps ++ sieve (top+2) where
+        top = min (n + 2^15) (2 + n*n - 4*n)
+        ps = takeWhile (\p -> p*p < top) (tail primes)
+
+factorizeBSmooth :: Integer -> Integer -> [Integer]
 factorizeBSmooth b = factors where
     factors m
-        | m == 1 || null fs = []
+        | m <= 1 || null fs = []
         | otherwise = head fs : factors (div m $ head fs)
-        where fs = [x | x <- [2..min b m], mod m x == 0]
+        where fs = [x | x <- takeWhile (<=b) primes, mod m x == 0]
 
 powMod m a b = iter 1 a b where
     a |* b = mod (a*b) m
@@ -57,9 +76,6 @@ powMod m a b = iter 1 a b where
         where r' = if odd b then r |* a else r
 
 sqrtMod m n = find (\r -> mod (r*r) m == n') [0..m-1] where n' = mod n m
-
-factorLim :: Int
-factorLim = 127
 
 productMod n = foldl (\a b -> mod (a*b) n) 1
 
@@ -70,33 +86,44 @@ fermatMethod n a b = (f1, f2) where
 
 -- Takes a list of candidates whose square modulo n could be smooth
 -- Yields (x, y), with x² = y² mod n
-mergeFactors n candidates = (x, y) where
-    factorize = factorizeBSmooth (toInteger factorLim)
+mergeFactors n candidates = traceShow (lines, cols) (x, y) where
+    b = ceiling . exp $ sqrt (0.5 * log n' * (log . log) n') where n' = fromInteger n
+
+    factorize = factorizeBSmooth (toInteger b)
     maybeFactors i = if product fs == j then Just (i, fs) else Nothing
         where j = mod (i*i) n; fs = factorize j
-    (roots, factors) = unzip
-        . take 63 -- The sign bit will break the algorithm
-        . map (second (map (\l -> (fromInteger . head $ l, length l)). group))
-        $ mapMaybe maybeFactors candidates
+    countFactors = map (\l -> (fromInteger . head $ l, length l)). group
+    zeroIndex :: [e] -> Array Int e
+    zeroIndex l = listArray (0, length l - 1) l
+    (roots, factors) = bimap zeroIndex zeroIndex
+        . unzip
+        . take (20 + lines)
+        . map (second countFactors)
+        $ mapMaybe maybeFactors candidates    
 
-    bounds = (0, factorLim);
-    bits = [0 .. length roots - 1] -- We assign each root to a bit
+    smoothPrimes = takeWhile (<=b) . map fromInteger $ primes
+    primeIdx :: UArray Int Int
+    primeIdx = array (0, b) (zip smoothPrimes [0..])
 
-    maybeOddExp s (p, k) = if odd k then Just (p, s) else Nothing
-    matrix :: UArray Int Int64
-    matrix = accumArray (\a -> xor a . bit) 0 bounds
+    lines = length smoothPrimes
+    cols = length roots - 1
+
+    maybeOddExp s (p, k) = if odd k then Just ((primeIdx ! p, s), True) else Nothing
+    matrix :: UArray (Int, Int) Bool
+    matrix = array ((0, 0), (lines, cols))
         . concatMap catMaybes
-        . zipWith (map . maybeOddExp) bits
-        $ factors
+        . zipWith (map . maybeOddExp) [0 .. cols]
+        $ elems factors
 
     solution = solve matrix
-    included = filter (testBit solution) bits
+    included = filter (solution!) [0 .. cols]
 
     factorCount :: UArray Int Int
-    factorCount = accumArray (+) 0 bounds includedFactors where
-        includedFactors = concatMap (factors !!) included
+    factorCount = accumArray (+) 0 (0, lines) includedFactors where
+        includedFactors = map (first (primeIdx !))
+            $ concatMap (factors !) included
 
-    x = productMod n $ map (roots !!) included
+    x = productMod n $ map (roots !) included
     y = productMod n
         . map ((uncurry (powMod n) . second (.>>. 1)) . first toInteger)
         $ assocs factorCount
